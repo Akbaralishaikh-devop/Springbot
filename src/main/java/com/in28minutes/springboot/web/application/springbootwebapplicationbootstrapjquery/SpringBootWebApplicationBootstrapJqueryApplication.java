0b7a3a3384a274c67d7dc5b8696b09af227e88f0
khalid Ansari <khalid.ansari@CTPL.in>
2023-02-10 17:28:57 +0530

    initial commit

---
+++ src/main/java/com/in28minutes/springboot/web/application/springbootwebapplicationbootstrapjquery/SpringBootWebApplicationBootstrapJqueryApplication.java
@@ -0,0 +1,21 @@
+package com.in28minutes.springboot.web.application.springbootwebapplicationbootstrapjquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class SpringBootWebApplicationBootstrapJqueryApplication {
+	
+	@GetMapping("/AWS")
+	public String HelloAWS()
+	{
+		return "Hello AWS Application";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplicationBootstrapJqueryApplication.class, args);
+	}
+
+
+}
